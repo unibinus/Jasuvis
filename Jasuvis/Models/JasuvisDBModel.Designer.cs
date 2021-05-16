a1406a45e0e5ce0a3dfbc294939a606046e73668
Ryan Vieri Kwa <ryan.kwa24@gmail.com>
2021-05-16 22:45:38 +0700

    Update DB Model

--- Jasuvis/Models/JasuvisDBModel.Designer.cs
+++ Jasuvis/Models/JasuvisDBModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'F:\Binus\Semester_4\SE\Jasuvis\Jasuvis-master\Jasuvis-master\Jasuvis\Models\JasuvisDBModel.edmx'. 
+﻿// T4 code generation is enabled for model 'F:\Binus\Semester_4\SE\Jasuvis\Jasuvis2\Jasuvis\Models\JasuvisDBModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- Jasuvis/Models/User.cs
+++ Jasuvis/Models/User.cs
@@ -24,12 +24,14 @@ namespace Jasuvis.Models
     
         public string UserID { get; set; }
         public string RoleID { get; set; }
+        public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string PhoneNumber { get; set; }
         public string Name { get; set; }
         public string Gender { get; set; }
         public string Address { get; set; }
+        public string ProfilePicture { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Review> Reviews { get; set; }
