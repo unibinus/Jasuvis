34807c5af83075f605c40f4fa51981358edc20b1
Evelin <evelin230601@gmail.com>
2021-05-25 15:47:09 +0700

    Make Service Provider Controller

---
+++ Jasuvis/Controllers/SPController.cs
@@ -0,0 +1,27 @@
+﻿using Jasuvis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Jasuvis.Controllers
+{
+    public class SPController
+    {
+        public static void serviceProviderData(User u)
+        {
+            User user = Handlers.UserHandler.getUserID(u.UserID);
+            Service service = Handlers.ServiceHandler.GetService();
+            ServiceType serviceType = Handlers.ServiceTypeHandler.GetType();
+
+            var newObj = new {
+                serviceUserID = service.UserID,
+                serviceTypeName = serviceType.ServiceTypeName,
+                servicename = service.ServiceName,
+                serviceProviderName = user.Name,
+                serviceProviderPhoto = user.ProfilePicture
+            };
+
+        }
+    }
+}
