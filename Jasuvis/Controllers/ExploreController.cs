67611f5197535d34c39353decbf8581954b11c6a
su clinton <suclinton34@gmail.com>
2021-06-13 16:58:23 +0700

    Update Controller

--- Jasuvis/Controllers/ExploreController.cs
+++ Jasuvis/Controllers/ExploreController.cs
@@ -9,6 +9,11 @@ namespace Jasuvis.Controllers
 {
     public class ExploreController
     {
+
+        public static string firsttype()
+        {
+            return Handlers.ServiceTypeHandler.GetFirstType();
+        }
         public static void serviceProviderData(Repeater spData)
         {
             List<Service> services = Handlers.ServiceHandler.getSP();
@@ -28,22 +33,61 @@ namespace Jasuvis.Controllers
                 };
                 recommendedList.Add(newObject);
             }
-            //foreach (Service service in services)
-            //{
-            //    var newObject = new
-            //    {
-            //        serviceid = service.ServiceID,
-            //        userid = service.UserID
-            //    };
-            //    newList.Add(newObject);
-            //}
             spData.DataSource = recommendedList;
             spData.DataBind();
+        }
+
+        public static void ProviderTypeData(Repeater spData, string type)
+        {
+            List<Service> services = Handlers.ServiceHandler.getSP();
+            List<User> serviceProvider = Handlers.UserHandler.getUserFromServiceList(services);
+            List<ServiceType> serviceTypes = Handlers.ServiceTypeHandler.getServiceTypeFromServiceList(services);
+
+            List<dynamic> recommendedList = new List<dynamic>();
+            for (int i = 0; i < serviceProvider.Count; i++)
+            {
+                if (serviceTypes[i].ServiceTypeName.Equals(type))
+                {
+                    var newObject = new
+                    {
+                        serviceUserID = services[i].UserID,
+                        serviceTypeName = serviceTypes[i].ServiceTypeName,
+                        servicename = services[i].ServiceName,
+                        serviceProviderName = serviceProvider[i].Name,
+                        serviceProviderPhoto = serviceProvider[i].ProfilePicture
+                    };
+                    recommendedList.Add(newObject);
+
+                }
+            }
+            spData.DataSource = recommendedList;
+            spData.DataBind();
+        }
+
+        internal static void serviceTypeData(object typeData)
+        {
+            throw new NotImplementedException();
         }
 
         internal static void serviceProviderData(object spData)
         {
             throw new NotImplementedException();
+        }
+        public static void serviceTypeData(Repeater typeData)
+        {
+            List<ServiceType> serviceTypes = Handlers.ServiceTypeHandler.getServiceType();
+
+            List<dynamic> recommendedList = new List<dynamic>();
+            for (int i = 0; i < serviceTypes.Count; i++)
+            {
+                var newObject = new
+                {
+                    serviceTypeName = serviceTypes[i].ServiceTypeName,
+                };
+                recommendedList.Add(newObject);
+            }
+            typeData.DataSource = recommendedList;
+            typeData.DataBind();
         }
     }
 }
--- Jasuvis/Controllers/SPController.cs
+++ Jasuvis/Controllers/SPController.cs
@@ -3,25 +3,69 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.UI.WebControls;
 
 namespace Jasuvis.Controllers
 {
     public class SPController
     {
-        public static void serviceProviderData(User u)
+        public static void serviceProviderData(User u, Repeater rep)
         {
             User user = Handlers.UserHandler.getUserID(u.UserID);
-            Service service = Handlers.ServiceHandler.GetService();
-            ServiceType serviceType = Handlers.ServiceTypeHandler.GetType();
-
-            var newObj = new {
+            Service service = Handlers.ServiceHandler.GetService(u);
+            ServiceType serviceType = Handlers.ServiceTypeHandler.GetType(service);
+            List<dynamic> recommendedList = new List<dynamic>();
+            var newObj = new
+            {
                 serviceUserID = service.UserID,
                 serviceTypeName = serviceType.ServiceTypeName,
                 servicename = service.ServiceName,
+                servicePrice = service.ServicePrice,
                 serviceProviderName = user.Name,
-                serviceProviderPhoto = user.ProfilePicture
+                serviceProviderPhoto = user.ProfilePicture,
+                serviceAddress = user.Address,
+                serviceDescription = service.ServiceDescription
             };
+            recommendedList.Add(newObj);
+            rep.DataSource = recommendedList;
+            rep.DataBind();
+        }
+        public static void serviceTransaction(User u, Repeater rep)
+        {
+            User user = Handlers.UserHandler.getUserID(u.UserID);
+            List<Transaction> transactions = Handlers.TransactionHandler.getTR();
 
+            List<dynamic> recommendedList = new List<dynamic>();
+            List<dynamic> TRList = new List<dynamic>();
+            List<Service> s = new List<Service>();
+            for (int i = 0; i < transactions.Count; i++)
+            {
+                if (transactions[i].UserID == u.UserID)
+                {
+                    var tr = transactions[i];
+                    TRList.Add(tr);
+                }
+
+            }
+            for (int i = 0; i < TRList.Count; i++)
+            {
+                string id = TRList[i].ServiceID;
+                Service st = Handlers.ServiceHandler.GetServiceByID(id);
+                User us = Handlers.UserHandler.getUserID(st.UserID);
+                ServiceType serviceType = Handlers.ServiceTypeHandler.GetType(st);
+                var newObject = new
+                {
+                    transactionID = TRList[i].TransactionID,
+                    serviceProviderName = st.ServiceName,
+                    serviceProviderPhoto = us.ProfilePicture,
+                    serviceTypeName = serviceType.ServiceTypeName,
+                    transactionDate = TRList[i].TransactionDate,
+                };
+                recommendedList.Add(newObject);
+
+            }
+            rep.DataSource = recommendedList;
+            rep.DataBind();
         }
     }
 }
